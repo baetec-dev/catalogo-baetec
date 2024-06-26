46e7bbb70247891f4fe3140df7a0d21eb1fc1baf
Baetec Inovação <socialmedia.baetec@gmail.com>
2024-06-26 13:49:48 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ function menuShow(){
         menuMobile.classList.add('open')
     }
 }
+
+function showModal(){
+    let dialog = document.querySelector("dialog");
+    dialog.showModal();
+}
+
+function closeModal(){
+    let dialog = document.querySelector("dialog");
+    dialog.close();
+}
